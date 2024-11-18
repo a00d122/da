5ce7d04bfffe53d2c6428d3ae0463f42723b1ea7
a00d122 <vladislavtugusi32@gmail.com>
2024-11-18 14:38:04 +0500

    l

---
+++ WishListApp-main/app/src/main/java/com/example/wishlistapp/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.wishlistapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFFF79BE)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFF6669EC)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF367C85)
